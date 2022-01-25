f3b37a47c40affa46661fba6d588080720f57e19
Sherpax <mvd0008@alu.medac.es>
2022-01-25 11:58:44 +0100

    Cambios en perfil

--- js/perfil.js
+++ js/perfil.js
@@ -1,3 +1,22 @@
+
+function editarCampos(){
+     $(".non-editable").each(function () {
+        $(this).attr("readonly", false);
+        $(this).css("border", "1px solid black");
+     });
+}
+
+function guardarCampos() {
+    $(".btn btn-info").attr("visibility", "hidden");
+    $(".non-editable").each(function () {
+        $(this).attr("readonly", true);
+        $(this).css("outline", "none");
+        $(this).css("border", "0px");
+    });
+    location.reload();
+}
+
+
 // JQuery para esconder y mostrar los botones de editar/guardar
 
 $(document).ready(function(){
@@ -17,5 +36,6 @@ $(document).ready(function(){
         $("#btn-guardar").show();
     });
 });
+
 
 
